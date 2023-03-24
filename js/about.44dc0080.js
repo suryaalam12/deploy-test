abb59661b7509bf5df23acc3cc0b78d46ea4dd96
suryaalam12 <andisuryaalam12@gmail.com>
2023-03-24 09:27:48 +0700

    deploy

---
+++ js/about.44dc0080.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkbuku"]=self["webpackChunkbuku"]||[]).push([[443],{5399:function(u,n,t){t.r(n),t.d(n,{default:function(){return i}});var s=function(){var u=this;u._self._c;return u._m(0)},e=[function(){var u=this,n=u._self._c;return n("div",{staticClass:"about"},[n("h1",[u._v("This is an about page")])])}],a=t(1001),l={},r=(0,a.Z)(l,s,e,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.44dc0080.js.map
